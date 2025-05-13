dd66e512579a18d216cfd1851135f4b9d74c347a
ChristopherScot <16506996+ChristopherScot@users.noreply.github.com>
2025-05-12 22:45:23 -0400

    POC with tons of crappy debug logs

--- Makefile
+++ Makefile
@@ -1,12 +1,3 @@
-build:
-	rm -f builds/create_link.zip
-	rm -f builds/create_link
-	cd lambdas/createlink && GOOS=linux GOARCH=arm64 go build -o bootstrap
-	cd lambdas/createlink && zip ../../builds/create_link.zip bootstrap && rm bootstrap
-	rm -f builds/get_links.zip
-	rm -f builds/get_links
-	cd lambdas/getlinks && GOOS=linux GOARCH=arm64 go build -o bootstrap
-	cd lambdas/getlinks && zip ../../builds/get_links.zip bootstrap && rm bootstrap
 
 apply:
 	cd infra && tofu apply
@@ -14,3 +5,24 @@ apply:
 tidy:
 	cd lambdas/createlink && go mod tidy
 	cd lambdas/getlinks && go mod tidy
+	cd lambdas/golinksbrowser && go mod tidy
+
+build_golinks_browser:
+	rm -f builds/go_links_browser.zip
+	rm -f builds/go_links_browser
+	cd lambdas/golinksbrowser && GOOS=linux GOARCH=arm64 go build -o bootstrap
+	cd lambdas/golinksbrowser && zip ../../builds/go_links_browser.zip bootstrap && rm bootstrap
+
+build_create_link:
+	rm -f builds/create_link.zip
+	rm -f builds/create_link
+	cd lambdas/createlink && GOOS=linux GOARCH=arm64 go build -o bootstrap
+	cd lambdas/createlink && zip ../../builds/create_link.zip bootstrap && rm bootstrap
+
+build_get_links:
+	rm -f builds/get_links.zip
+	rm -f builds/get_links
+	cd lambdas/getlinks && GOOS=linux GOARCH=arm64 go build -o bootstrap
+	cd lambdas/getlinks && zip ../../builds/get_links.zip bootstrap && rm bootstrap
+
+build: build_create_link build_get_links build_golinks_browser
--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,13 @@ variable "token" {
 
 provider "aws" {
   region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = "Production"
+      Project     = "URLShortener"
+    }
+  }
 }
 
 resource "aws_s3_bucket" "redirects" {
@@ -28,21 +35,7 @@ resource "aws_s3_bucket_website_configuration" "redirects" {
   }
 }
 
-resource "aws_s3_bucket_policy" "public_read" {
-  bucket = aws_s3_bucket.redirects.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.redirects.arn}/go/*"
-      }
-    ]
-  })
-}
 resource "aws_iam_role_policy" "lambda_logging" {
   name   = "create-link-lambda-logging-policy"
   role   = aws_iam_role.lambda_exec.id
@@ -83,7 +76,7 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-resource "aws_lambda_function" "create_redirect" {
+resource "aws_lambda_function" "create_link" {
   filename         = "../builds/create_link.zip"
   function_name    = "create-link"
   role             = aws_iam_role.lambda_exec.arn
@@ -171,4 +164,108 @@ resource "aws_iam_role_policy" "lambda_dynamodb_get_access" {
       }
     ]
   })
+}
+
+resource "aws_lambda_function" "go_links_browser" {
+  filename         = "../builds/go_links_browser.zip" # Path to the zipped Lambda code
+  function_name    = "go-links-browser"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "bootstrap"
+  runtime          = "provided.al2023"
+  architectures    = ["arm64"]
+
+  source_code_hash = filebase64sha256("../builds/go_links_browser.zip")
+
+  environment {
+    variables = {
+      CREATE_LINK_LAMBDA = aws_lambda_function.create_link.function_name
+      GET_LINKS_LAMBDA   = aws_lambda_function.get_links.function_name
+      TOKEN              = var.token
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "redirects_bucket_policy" {
+  bucket = aws_s3_bucket.redirects.id
+  # For actual use restrict to your subnet's CIDR block or your NAT's IP
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+    {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.redirects.arn}/go/*"
+      },
+    ]
+  })
+
+  depends_on = [ 
+    aws_s3_bucket.redirects, 
+    ]
+}
+
+resource "aws_iam_role_policy" "lambda_invoke_permissions" {
+  name   = "webpage-lambda-invoke-policy"
+  role   = aws_iam_role.lambda_exec.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "lambda:InvokeFunction"
+        Resource = [
+          aws_lambda_function.create_link.arn,
+          aws_lambda_function.get_links.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_apigatewayv2_api" "go_links_browser_api" {
+  name          = "go-links-browser-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "go_links_browser_integration" {
+  api_id             = aws_apigatewayv2_api.go_links_browser_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.go_links_browser.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "go_links_browser_route" {
+  api_id    = aws_apigatewayv2_api.go_links_browser_api.id
+  route_key = "GET /"
+
+  target = "integrations/${aws_apigatewayv2_integration.go_links_browser_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "go_links_browser_post_route" {
+  api_id    = aws_apigatewayv2_api.go_links_browser_api.id
+  route_key = "POST /"
+
+  target = "integrations/${aws_apigatewayv2_integration.go_links_browser_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "go_links_browser_options_route" {
+  api_id    = aws_apigatewayv2_api.go_links_browser_api.id
+  route_key = "OPTIONS /"
+
+  target = "integrations/${aws_apigatewayv2_integration.go_links_browser_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "go_links_browser_stage" {
+  api_id      = aws_apigatewayv2_api.go_links_browser_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "go_links_browser_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.go_links_browser.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.go_links_browser_api.execution_arn}/*"
 }
